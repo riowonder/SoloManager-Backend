740c1e67b86897e55103ebe5c421c2cf1420fb87
Sumitbose05 <bosesomen062@gmail.com>
2025-06-23 00:28:12 +0530

    Updated CORS code

--- app.js
+++ app.js
@@ -8,42 +8,28 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://192.168.1.6:5173",
-  "https://solo-manager-frontend.vercel.app/"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
-// Preflight handler for CORS
-app.options('*', cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CORS Configuration
+const corsOptions = {
+  origin : "https://solo-manager-frontend.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
+};
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+//   "http://192.168.1.6:5173",
+//   "https://solo-manager-frontend.vercel.app/"
+// ];
+
+
+// Apply CORS before routes
+app.use(cors(corsOptions));
+
 
 // Connect to MongoDB
 connectDB();

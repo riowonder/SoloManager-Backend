aa86e5221e26789cd8ed901bd83cfee33c8a9f93
Sumitbose05 <bosesomen062@gmail.com>
2025-09-26 16:06:06 +0530

    cron fix 1

--- api/cron/subscriptionCron.js
+++ api/cron/subscriptionCron.js
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
   await connectDB();
   const now = new Date();
 
+  console.log("CRON job started")
+
   try {
     // 1️⃣ Handle expired subscriptions
     const expiredSubs = await Subscription.find({

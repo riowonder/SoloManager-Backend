45a972018c97ac7d809eb99b03d5808fb098f0ed
Sumitbose05 <bosesomen062@gmail.com>
2025-09-26 12:11:25 +0530

    route testing

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { sendExpiryMessage } from '../services/whatsappService.js';
 
 const router = express.Router();
-router.get('/send-test-message', sendExpiryMessage);
+router.get('/send-test-message', sendExpiryMessage);    
+
+router.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
 
 export default router;

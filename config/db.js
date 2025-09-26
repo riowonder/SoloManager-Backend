1e72924ec00bf4b714dd5c39052796f15e73166d
Sumitbose05 <bosesomen062@gmail.com>
2025-09-26 14:56:20 +0530

    upgraded to newer version

--- config/db.js
+++ config/db.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000, // timeout after 10s if cannot connect
     });
     console.log("✅ MongoDB connected");
